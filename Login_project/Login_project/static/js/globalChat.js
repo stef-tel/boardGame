40ff6324681bacf83aee28134077b2114c7f6829
stef-tel <stef.tellier@gmail.com>
2020-04-23 03:35:41 +0200

    chat is pretty Ok now !!

--- Login_project/Login_project/static/js/globalChat.js
+++ Login_project/Login_project/static/js/globalChat.js
@@ -1,77 +1,82 @@
 'use strict';
 
 var socket;
-    $(document).ready(function(){
-        //socket = io.connect('http://' + document.domain + ':' + location.port + '/chat');
-        socket = io.connect('http://127.0.0.1:5000/globalChat');
-        socket.on('connect', function() {
+var idElementCounter = 1;
+var newElementId;
+
+
+$(document).ready(function() {
+    //socket = io.connect('http://' + document.domain + ':' + location.port + '/chat');
+    socket = io.connect('http://127.0.0.1:5000/globalChat');
+    socket.on('connect', function() {
         socket.emit('joined', {});
     });
 
     socket.on('status', function(data) {
-              //$('#chat').val($('#chat').val() + '<' + data.msg + '>\n');
-              //$('#chat').scrollTop($('#chat')[0].scrollHeight);
-              addChat(data.msg,data.sender,'join');
+        addElement(data.msg, data.sender, "text-align: center;","notification is-danger");
     });
 
     socket.on('message', function(data) {
-        //$('#chat').val($('#chat').val() + data.msg + '\n');
-        //$('#chat').scrollTop($('#chat')[0].scrollHeight);
-        //var messagecontent = JSON.parse(data)
-        addChat(data.msg,data.sender,'message');
+        addChat(data.msg, data.sender);
     });
 
     $('#text').keypress(function(e) {
-            var code = e.keyCode || e.which;
-            if (code == 13) {
-                var text = $('#text').val();
-                $('#text').val('');
-                socket.emit('text', {msg: text});
-            }
-        });
-    });
-
-
-    function leave_room() {
-        socket.emit('left', {}, function() {
-            socket.disconnect();
-
-            // go back to the login page
-            window.location.href = "{{ url_for('main.index') }}";
-        });
-    }
-
-    function addChat (message, sender, type) {
-        // crée un nouvel élément div
-        var newDiv = document.createElement("div");
-    
-        var align;
-        var color;
-    
-        if (type == 'message'){
-            if (username == sender){
-            align = "text-align: right;"
-            color = "notification is-info"
-            // et lui donne un peu de contenu
-            newDiv.innerHTML = message;
-
-            } else {
-            align = "text-align: left;"
-            color = "notification is-warning"
-            newDiv.innerHTML = '<i><sup>' + sender + '</i></sup></br>' + message;
-            }
-        }else{
-            align = "text-align: center;"
-            color = "notification is-danger" 
-            newDiv.innerHTML = message;
+        var code = e.keyCode || e.which;
+        if (code == 13) {
+            var text = $('#text').val();
+            $('#text').val('');
+            socket.emit('text', {
+                msg: text
+            });
         }
+    });
+});
 
-        newDiv.setAttribute('class', color);
-        newDiv.setAttribute('style', align);
-    
-        // ajoute le nouvel élément créé et son contenu dans le DOM
-        var currentDiv = document.getElementById('parent');
-        currentDiv.insertBefore(newDiv, null);
-        currentDiv.scrollIntoView(false)
 
+function leave_room() {
+    socket.emit('left', {}, function() {
+        socket.disconnect();
+
+        // go back to the login page
+        window.location.href = "{{ url_for('main.index') }}";
+    });
+}
+
+
+
+function addElement(message, sender, style, classe, parentElement="parent") {
+    // crée un nouvel élément div
+    var newDiv = document.createElement("div");
+    newDiv.id  = 'newDiv' + (++idElementCounter);
+    newDiv.innerHTML = message;
+
+    newDiv.setAttribute('class', classe);
+    newDiv.setAttribute('style', style);
+
+    // ajoute le nouvel élément créé et son contenu dans le DOM
+    var currentDiv = document.getElementById(parentElement);
+    currentDiv.insertBefore(newDiv, null);
+    newDiv.scrollIntoView(false)
+
+    return newDiv.id
+
+}
+
+function addChat(message, sender) {
+    var New1;
+    var New2;
+
+    if (username == sender) { 
+        New1 = addElement("", sender, "", "columns is-multiline");
+        New2 = addElement("", sender, "", "column is-two-thirds", New1);
+        addElement(message, sender, "text-align: left;", "notification is-info", New2);
+        addElement("", sender, "", "column", New1);
     }
+    else{
+        New1 = addElement("", sender, "", "columns is-multiline");
+        addElement("", sender, "", "column", New1);
+        New2 = addElement("", sender, "", "column is-two-thirds", New1);
+        addElement('<i><sup>' + sender + '</i></sup></br>' + message, sender, "text-align: right;", "notification is-warning", New2);
+    }
+
+}
